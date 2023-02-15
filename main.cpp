8f2a8afb444e7b7524012339237daad8c731b389
zdamm001 <zdamm001@ucr.edu>
2023-02-14 22:51:25 -0800

    Setting up file io

--- main.cpp
+++ main.cpp
@@ -1,4 +1,35 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+//#include <pair>
+
+using namespace std;
+
 int main() {
+
+
+
+
+
+
+
+
+
+    string shipName = "ShipCase5.txt";
+    ifstream fin(shipName);
+    if (!fin.is_open()) {
+        cout << "Error opening " << shipName << "!" << endl;
+        exit(EXIT_FAILURE);
+    }
+    string coordinates, weight, description;
+    while (fin >> coordinates) {
+        
+    }
+    fin.close();
+
+
+
+
 
     return 0;
 }
