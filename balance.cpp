ba9131931a3025aeb907d93afbab47916dec5160
zdamm001 <zdamm001@ucr.edu>
2023-03-03 21:08:22 -0800

    Updated heuristic

--- balance.cpp
+++ balance.cpp
@@ -55,7 +55,7 @@ int top_container_buffer(vector<vector<container> >& , int);
 int top_container_between(vector<vector<container> >&, int, int);
 int top_container_buffer_between(vector<vector<container> >&, int, int);
 void a_star_search(priority_queue<node*, vector<node*>, CompareNode>&, vector<node*>&);
-int balance_heuristic(vector<vector<container> >&, int);
+int balance_heuristic(vector<vector<container> >&);
 
 
 struct container {
@@ -712,7 +712,7 @@ node* general_search(vector<vector<container> >& containers) {
     priority_queue<node*, vector<node*>, CompareNode> nodes;
     node *initial_state = new node(containers);
     initial_state->set_gn(0);
-    initial_state->set_hn(balance_heuristic(initial_state->containers, initial_state->totalTime));
+    initial_state->set_hn(balance_heuristic(initial_state->containers));
     nodes.push(initial_state);
     unsigned max_queue_size = 1;
     unsigned nodes_expanded = 0;
@@ -959,19 +959,19 @@ int top_container_buffer_between(vector<vector<container> >& buffer, int column1
 void a_star_search(priority_queue<node*, vector<node*>, CompareNode>& nodes, vector<node*>& new_nodes) {
     for (unsigned i = 0; i < new_nodes.size(); ++i) {
         new_nodes[i]->set_gn(new_nodes[i]->totalTime);
-        new_nodes[i]->set_hn(balance_heuristic(new_nodes[i]->containers, new_nodes[i]->totalTime));
+        new_nodes[i]->set_hn(balance_heuristic(new_nodes[i]->containers));
         nodes.push(new_nodes[i]);
     }
 }
 
-int balance_heuristic(vector<vector<container> >& containers, int totalTime) {
+int balance_heuristic(vector<vector<container> >& containers) {
     int heavier_side_weight = left_mass(containers);
     int lighter_side_weight = right_mass(containers);
     if (lighter_side_weight > heavier_side_weight) {
         swap(lighter_side_weight, heavier_side_weight);
     }
     int hn_weight = heavier_side_weight * 0.9 - lighter_side_weight;
-    return hn_weight * 0.5 + totalTime * 0.5;
+    return hn_weight;
 }
 
 
